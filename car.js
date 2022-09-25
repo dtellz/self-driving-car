631f9f00bca5fe5efac11d6b570472e3be7483c4
Diego <dtellez95@gmail.com>
2022-09-25 19:53:36 +0100

    implement detection sensor with unknown bug

--- car.js
+++ car.js
@@ -11,11 +11,13 @@ class Car {
         this.friction = 0.05;
         this.angle = 0;
 
+        this.sensor = new Sensor(this);
         this.controls = new Controls();
     }
 
-    update() {
+    update(roadBorders) {
         this.#move();
+        this.sensor.update(roadBorders);
     }
 
     #move() {
@@ -57,6 +59,7 @@ class Car {
         ctx.save();
         ctx.translate(this.x, this.y);
         ctx.rotate(-this.angle);
+
         ctx.beginPath();
         ctx.rect(
             -this.width / 2,
@@ -67,5 +70,7 @@ class Car {
         ctx.fill();
 
         ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 }
--- main.js
+++ main.js
@@ -9,10 +9,16 @@ car.draw(ctx);
 animate();
 
 function animate() {
-    car.update();
+    car.update(road.borders);
+
     canvas.height = window.innerHeight;
+
+    ctx.save();
+    ctx.translate(0, -car.y + canvas.height * 0.7);
+
     road.draw(ctx);
     car.draw(ctx);
-    requestAnimationFrame(animate);
 
+    ctx.restore();
+    requestAnimationFrame(animate);
 }
