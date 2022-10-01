b33681e2e9e88aae9e11d9d3af807772c669d1ad
Diego <dtellez95@gmail.com>
2022-10-01 11:32:54 +0100

    prepare neuralnetwork to handle layers

--- network.js
+++ network.js
@@ -1,6 +1,24 @@
 class NeuralNetwork {
     constructor(neuronCounts) {
         this.levels = [];
+        for (let i = 0; i < neuronCounts.length - 1; i++) {
+            this.level.push(new Level(
+                neuronCounts[i], neuronCounts[i + 1]
+            ))
+        }
+    }
+
+    static feedForward(givenInputs, network) {
+        let outputs = Level.feedForward(
+            givenInputs, network.level[0]
+        );
+        for (let i = 0; i < network.levels.length; i++) {
+            outputs = Level.feedForward(
+                outputs, network.level[i]
+            );
+            return outputs;
+
+        }
     }
 }
 
