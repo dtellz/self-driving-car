47be3640483b61246d5057bf1027e830104365a2
Diego <dtellez95@gmail.com>
2022-10-01 19:02:02 +0100

    add multiple simulations to catch the best

--- main.js
+++ main.js
@@ -8,33 +8,55 @@ networkCanvas.width = 600;
 const carCtx = carCanvas.getContext("2d");
 const networkCtx = networkCanvas.getContext("2d");
 const road = new Road(carCanvas.width / 2, carCanvas.width * 0.9)
-const car = new Car(road.getLaneCenter(1), 100, 30, 50, "AI");
+const cars = generateCars(100);
 const traffic = [
     new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)
 ]
 
 animate();
 
-function animate() {
+
+
+function generateCars(N) {
+    const cars = [];
+
+    for (let i = 0; i <= N; i++) {
+        cars.push(new Car(road.getLaneCenter(1), 100, 30, 50, "AI"));
+    }
+    return cars;
+}
+
+function animate(time) {
     for (let i = 0; i < traffic.length; i++) {
         traffic[i].update(road.borders, []);
     }
-    car.update(road.borders, traffic);
+    for (let j = 0; j < cars.length; j++) {
+        cars[j].update(road.borders, traffic);
+    }
+
+    const bestCar = cars.find(c => c.y === Math.min(...cars.map(c => c.y)));
 
     carCanvas.height = window.innerHeight;
     networkCanvas.height = window.innerHeight;
 
     carCtx.save();
-    carCtx.translate(0, -car.y + carCanvas.height * 0.7);
+    carCtx.translate(0, -bestCar.y + carCanvas.height * 0.7);
 
     road.draw(carCtx);
     for (let i = 0; i < traffic.length; i++) {
         traffic[i].draw(carCtx, "red");
     }
-    car.draw(carCtx, "blue");
+    carCtx.globalAlpha = 0.2;
+    for (let k = 0; k < cars.length; k++) {
+        cars[k].draw(carCtx, "blue");
+    }
+    carCtx.globalAlpha = 1;
+    bestCar.draw(carCtx, "blue", true)
 
     carCtx.restore();
 
-    Visualizer.drawNetwork(networkCtx, car.brain);
+    networkCtx.lineDashOffset = -time / 50;
+
+    Visualizer.drawNetwork(networkCtx, bestCar.brain);
     requestAnimationFrame(animate);
 }
